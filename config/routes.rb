c89f11587569ac35f9155d89817b08591094e221
adna <adna@adna.com>
2018-07-24 23:59:59 +0800

    Add app landing page and user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root "main#index"
+
   devise_for :users
 
   resources :users, only: [:index, :show] do
@@ -6,5 +9,7 @@ Rails.application.routes.draw do
   end
 
   resources :tables, only: [:show, :edit, :update, :destroy]
+
+  delete '/tables/delete', to: 'tables#delete', as: 'tables'
 
 end
