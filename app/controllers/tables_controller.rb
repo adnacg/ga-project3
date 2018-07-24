d0724cef511e139d2c2f2413ad5516f18c866346
adna <adna@adna.com>
2018-07-24 22:37:55 +0800

    Refactor

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -1,53 +1,69 @@
 class TablesController < ApplicationController
-<<<<<<< HEAD
- 
-=======
->>>>>>> master
   before_action :authenticate_user!
 
+  def check_user
+    if params[:user_id]
+      redirect_to('/') if current_user.id != params[:user_id].to_i
+    end
+  end
+
+  before_action :check_user
+
   def index
-    @current_user = User.find_by_id(current_user.id)
-    @tables = @current_user.tables
+    # route: /users/:user_id/tables
+    # method/controller: user_tables GET => tables#index
+    @user = User.find(params[:user_id])
+    @tables = @user.tables
   end
 
   def show
-    @current_user = User.find_by_id(current_user.id)
-    @tables = @current_user.tables
-    @showTable = @tables.find(params[:id])
+    # route: /tables/:id
+    # method/controller: table GET => tables#show
+    @table = Table.find(params[:id])
+    if @table.user_id != current_user.id
+      redirect_to ('/')
+    end
   end
 
   def new
-    # /users/:user_id/tables/new
-    # new_user_table GET => tables#new
+    # route: /users/:user_id/tables/new
+    # method/controller: new_user_table GET => tables#new
   end
 
   def create
-    # /users/:user_id/tables
-    # user_tables POST => tables#create
+    # route: /users/:user_id/tables
+    # method/controller: user_tables POST => tables#create
     new_table = Table.new(table_params)
-    new_table.user = current_user
+    new_table.user = User.find(params[:user_id])
     new_table.is_free = true
     if new_table.save
-      redirect_to new_user_table
+      redirect_to new_table
     else
       render 'new'
     end
   end
 
   def edit
-    @table = Table.find_by(:user_id => params[:user_id], :id=> params[:id])
+    # route: /tables/:id/edit
+    # method/controller: edit_table GET => tables#edit
+    @table = Table.find(params[:id])
   end
 
   def update
-    @table = Table.find_by(:user_id => params[:user_id], :id=> params[:id])
-    @table.update(table_params)
-    redirect_to user_table_path(@table)
+    # route: /tables/:id
+    # method/controller: table PATCH => tables#update
+    table = Table.find(params[:id])
+    table.update(table_params)
+    redirect_to table
   end
 
   def destroy
-    @table = Table.find_by(:user_id => params[:user_id], :id => params[:id])
-    @table.destroy
-    redirect_to user_tables
+    # route: /tables/:id
+    # method/controller: table DELETE => tables#destroy
+    table = Table.find(params[:id])
+    user = table.user
+    table.destroy
+    redirect_to user_tables_path(user)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:index, :show] do
-    resources :tables, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+    resources :tables, only: [:index, :new, :create]
   end
+
+  resources :tables, only: [:show, :edit, :update, :destroy]
 
 end
