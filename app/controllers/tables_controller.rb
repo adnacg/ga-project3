426733b940e49cc5bdef1dd61a75140cb9f3d10d
Lee Wen Yao <peterleewenyao@gmail.com>
2018-07-24 16:48:37 +0800

    defined update function in controller

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -16,7 +16,9 @@ class TablesController < ApplicationController
   end
 
   def update
-    @table = Table.update(table_params)
+    @table = Table.find_by(:user_id => params[:user_id], :id=> params[:id])
+    @table.update(table_params)
+    redirect_to user_table_path(@table)
   end
 
   def destroy
@@ -25,6 +27,6 @@ class TablesController < ApplicationController
   private
 
   def table_params
-    
+    params.require(:tables).permit(:seater, :is_free)
   end
 end
