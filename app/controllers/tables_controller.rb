6769d166064ccb832c510f17d1bffec85bcdf292
Lee Wen Yao <peterleewenyao@gmail.com>
2018-07-24 16:34:37 +0800

    defined edit action in controller

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -12,11 +12,19 @@ class TablesController < ApplicationController
   end
 
   def edit
+    @table = Table.find_by(:user_id => params[:user_id], :id=> params[:id])
   end
 
   def update
+    @table = Table.update(table_params)
   end
 
   def destroy
+  end
+
+  private
+
+  def table_params
+    
   end
 end
