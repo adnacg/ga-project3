23faf83a4edc4f20b118be6709292b725e4dee21
adna <adna@adna.com>
2018-07-25 16:29:34 +0800

    Edit user show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @tables = @user.tables
+    @two_seaters_free = @tables.where('seater': 2, 'is_free': true)
+    @two_seaters_total = @tables.where('seater': 2).count
+    @four_seaters_free = @tables.where('seater': 4, 'is_free': true)
+    @four_seaters_total = @tables.where('seater': 4).count
+    @six_seaters_free = @tables.where('seater': 6, 'is_free': true)
+    @six_seaters_total = @tables.where('seater': 6).count
   end
 end
