c8d7550f2880bde1d0de0e0bbc966ccb2be689dd
saywandeusher <ongsaywan@hotmail.com>
2018-07-25 16:21:11 +0800

    complete patrons_controller,show & edit

--- app/controllers/patrons_controller.rb
+++ app/controllers/patrons_controller.rb
@@ -1,13 +1,36 @@
 class PatronsController < ApplicationController
+  before_action :authenticate_patron!
+
+  def check_patron
+    if params[:id]
+      redirect_to('/') if current_patron.id != params[:id].to_i
+    end
+  end
+
+  before_action :check_patron
+
   def show
+    @patron = Patron.find(params[:id])
   end
 
   def edit
+    @patrons = Patron.find(params[:id])
   end
 
   def update
+    patron = Patron.find(params[:id])
+    patron.update(patron_params)
+    redirect_to patron
   end
 
   def destroy
+    patron = Patron.find(params[:id])
+    patron.destroy
+    redirect_to root_path
+  end
+
+  private
+  def patron_params
+    params.require(:patron).permit(:name, :email)
   end
 end
