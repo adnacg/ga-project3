c6a2c2c3612d7e5e04269b8d7a00bab2bbf6d673
saywandeusher <ongsaywan@hotmail.com>
2018-07-26 23:15:11 +0800

    Done with index page layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resource)
         if resource.class.name == "Patron"
             current_patron
-        elsif resource.classe.name == "User"
+        elsif resource.class.name == "User"
             user_tables_url(user)
         end
     end
