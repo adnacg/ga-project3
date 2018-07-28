5fd137b75b2c52925182ef2a7d5924906fb250a6
adna <adna@adna.com>
2018-07-28 16:59:34 +0800

    Edit profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resource)
         if resource.class.name == "Patron"
-            current_patron
+            # current_patron
+            patron_favourites_url(current_patron)
         elsif resource.class.name == "User"
             user_tables_url(current_user)
         end
